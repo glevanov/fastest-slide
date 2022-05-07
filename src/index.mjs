3a87c458cb2adc794f92b849c930d2f95edd6b08
Grigoriy Levanov <gregory@levanov.io>
2022-05-07 18:14:15 +0300

    feat: add graph constructor & jsdoc annotations

--- src/index.mjs
+++ src/index.mjs
@@ -1,7 +1,56 @@
 import { resolve } from 'node:path';
 
-import { readFile } from './readFile.mjs';
+import { IdProvider, readFile } from './helpers.mjs';
 
 const path = resolve('tests/fixtures/1.txt');
 
-console.log(readFile(path));
+/**
+ *
+ * @param lines {string[]}
+ * @returns {{layers: number[], layersNumber: number}}
+ */
+const parseLines = (lines) => {
+	const [layersNumber, ...layers] = lines;
+	const parsedLayers = layers.map((layer) => layer.split(' ').map(Number));
+	return {
+		layersNumber: Number(layersNumber),
+		layers: parsedLayers,
+	};
+};
+
+/**
+ * @typedef GraphNode
+ * @type {object}
+ * @property {number} weight
+ * @property {number[]} connections
+ */
+
+/**
+ * @typedef Graph
+ * @type {Object.<number,GraphNode>}
+ */
+
+/**
+ * @param layers {number[]}
+ * @returns {Graph}
+ */
+const makeGraph = (layers) => {
+	const idProvider = new IdProvider();
+	const graph = {};
+	layers.forEach((layer, layerIndex) => {
+		layer.forEach((node, nodeIndex) => {
+			const isLastLayer = layerIndex === layers.length - 1;
+			const nextLayer = layers[layerIndex + 1];
+			const connections = isLastLayer ? null : [nextLayer[nodeIndex], nextLayer[nodeIndex + 1]];
+			graph[idProvider.getNext()] = {
+				weight: node,
+				connections,
+			};
+		});
+	});
+	return graph;
+};
+
+const parsed = parseLines(readFile(path));
+const graph = makeGraph(parsed.layers)
+console.log(graph);
--- src/readFile.mjs
+++
@@ -1,8 +0,0 @@
-import { readFileSync } from 'node:fs';
-
-export const readFile = (path) => {
-	const data = readFileSync(path, 'UTF-8');
-	const lines = data.split(/\r?\n/);
-
-	return lines;
-};
